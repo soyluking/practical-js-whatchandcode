964aca752ab41b92ab9ef732dfbebf74a47dd44d
Luigi Pizarro <soyluking@gmail.com>
2017-03-30 17:35:03 -0300

    Avances de la version 8

--- script.js
+++ script.js
@@ -61,13 +61,11 @@ var todoList = {
   }
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function() {
-  todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener('click', function() {
-  todoList.toggleAll();
-});
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+};
